abacdc54c4c522ebd9b1e6a0aa53de1e3c6af4af
Rahul Prasad <rpsp981@gmail.com>
2021-12-06 01:02:35 +0530

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
-import React, { MouseEvent, useEffect, useRef, useState } from "react";
+import React, { MouseEvent, MutableRefObject, useCallback, useEffect, useRef, useState } from "react";
 import "./App.css";
-import { Create, Logo, MoreOptions, NotificationBell, SearchIcon, YoutubeApps } from "./assets";
+import {
+	Create,
+	ForwardCounter,
+	Logo,
+	MoreOptions,
+	NotificationBell,
+	PreviousCounter,
+	SearchIcon,
+	YoutubeApps,
+} from "./assets";
 import { data } from "./data";
 import { sliderData } from "./data";
 
@@ -11,17 +20,36 @@ const App: React.FC = () => {
 		}
 	};
 
-	const sliderRef = useRef(null);
 	const [leftVisible, setLeftVisible] = useState(false);
 	const [rightVisible, setRightVisible] = useState(true);
+	const sliderRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
 
-	// useEffect(() => {
-	// 	const currentRef = sliderRef.current;
-	// 	if (currentRef === null) return;
-	// 	else {
-	// 		const listener = currentRef.addEventListener("scroll", () => {});
-	// 	}
-	// });
+	useEffect(() => {
+		const currentRef = sliderRef.current;
+		if (!currentRef) return;
+
+		if (currentRef) {
+			currentRef.addEventListener("scroll", () => {
+				const end = currentRef.scrollWidth - currentRef.getBoundingClientRect().width - 8;
+				console.log(end);
+				setLeftVisible(currentRef.scrollLeft !== 0);
+				setRightVisible(currentRef.scrollLeft <= end);
+			});
+		}
+	}, [sliderRef]);
+
+	const moveLeft = useCallback(() => {
+		if (!sliderRef.current) return;
+		sliderRef.current.scrollLeft -= 200;
+		console.log("movedLeft");
+	}, [sliderRef]);
+
+	const moveRight = useCallback(() => {
+		if (!sliderRef.current) return;
+		sliderRef.current.scrollLeft += 200;
+		console.log("movedRight");
+	}, [sliderRef]);
+
 	return (
 		<div className="App">
 			<div className="header">
@@ -127,42 +155,41 @@ const App: React.FC = () => {
 					</div>
 					<div className="copyright">© 2021 Google LLC</div>
 				</div>
-				<div style={{ display: "flex", flexDirection: "column" }}>
-					<div
-						className="slider"
-						style={{
-							display: "flex",
-							alignItems: "center",
-							width: "calc(100vw - 240px)",
-							height: "55px",
-							borderTop: "1px solid grey",
-							borderBottom: "1px solid grey",
-						}}>
-						{sliderData.map((i) => {
-							return (
-								<div
-									style={{
-										display: "flex",
-										alignItems: "center",
-										paddingLeft: "10px",
-										paddingRight: "10px",
-										whiteSpace: "nowrap",
-										textOverflow: "ellipsis",
-										height: "35px",
-										color: "white",
-										border: "1px solid rgba(93, 93, 93, 0.5)",
-										borderRadius: "110px",
-										marginLeft: "25px",
-										backgroundColor: "rgba(93, 93, 93, 0.5)",
-									}}>
-									{i}
-								</div>
-							);
-						})}
+				<div className="rightCol" style={{ display: "flex", flexDirection: "column" }}>
+					<div className="sliderContainer" style={{ display: "flex", alignItems: "center" }}>
+						{leftVisible && (
+							<img onClick={moveLeft} className="backwardCounter" src={PreviousCounter} alt="Previous" />
+						)}
+						<div
+							ref={sliderRef}
+							className="slider"
+							style={{
+								display: "flex",
+							}}>
+							{sliderData.map((item) => {
+								return (
+									<div
+										className="sliderItem"
+										style={{
+											display: "flex",
+										}}>
+										{item}
+									</div>
+								);
+							})}
+						</div>
+						{rightVisible && (
+							<img onClick={moveRight} className="forwardCounter" src={ForwardCounter} alt="Next" />
+						)}
 					</div>
 					<div
-						className="rightCol"
-						style={{ display: "flex", height: "calc(100vh - 56px)", width: "calc(100vw - 240px)" }}>
+						className="container"
+						style={{
+							display: "flex",
+							height: "calc(100vh - 114px)",
+							width: "calc(100vw - 240px)",
+							overflowY: "hidden",
+						}}>
 						<div
 							className="videoListContainer"
 							style={{
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,23 +1,25 @@
-import { ReactComponent as MoreOptions } from './svg/more-options.svg';
-import {ReactComponent as Logo} from './svg/logo.svg'
+import {ReactComponent as MoreOptions } from './svg/more-options.svg';
+import {ReactComponent as Logo} from './svg/logo.svg';
 import {ReactComponent as SearchIcon} from './svg/searchIcon.svg';
-import  HomeButton from './svg/homeButton.svg'
-import  ExploreButton from './svg/explore.svg'
-import  Subscribe from './svg/subscriptions.svg'
-import  Library from './svg/library.svg'
-import  History from './svg/history.svg'
-import  PersonalVideos from './svg/Personal-videos.svg'
-import  WatchLater from './svg/watch_later.svg'
-import  SavedSongs from './svg/favourites.svg'
-import  ShowMore from './svg/showMore.svg'
+import {ReactComponent as NotificationBell} from './svg/notificationsIcon.svg';
+import {ReactComponent as YoutubeApps} from './svg/youtubeApps.svg';
+import {ReactComponent as Create} from './svg/createVideo.svg';
+import  PreviousCounter from './svg/chevron-back-circle.svg';
+import  ForwardCounter from './svg/chevron-forward-circle.svg';
+import  HomeButton from './svg/homeButton.svg';
+import  ExploreButton from './svg/explore.svg';
+import  Subscribe from './svg/subscriptions.svg';
+import  Library from './svg/library.svg';
+import  History from './svg/history.svg';
+import  PersonalVideos from './svg/Personal-videos.svg';
+import  WatchLater from './svg/watch_later.svg';
+import  SavedSongs from './svg/favourites.svg';
+import  ShowMore from './svg/showMore.svg';
 import Premium from './svg/premiumLogo.svg';
 import Settings from './svg/settings.svg';
 import ReportHistory from './svg/reportHistory.svg';
 import Help from './svg/help.svg';
 import Feedback from './svg/feedback.svg';
-import {ReactComponent as Create} from './svg/createVideo.svg';
-import {ReactComponent as YoutubeApps} from './svg/youtubeApps.svg';
-import {ReactComponent as NotificationBell} from './svg/notificationsIcon.svg';
 
 export {
     MoreOptions,
@@ -39,5 +41,7 @@ export {
     SearchIcon,
     Create,
     YoutubeApps,
-    NotificationBell
+    NotificationBell,
+    PreviousCounter,
+    ForwardCounter
 }
